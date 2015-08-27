08239ce7fb4352a4a5838869bdbed92a8952e567
Mateusz Łukomski <matrix.disc@gmail.com>
2015-08-27 09:10:36 +0200

    Removed Camera3D translation from update

--- MGWBGraphics/MatrixEngine/Camera3D.cpp
+++ MGWBGraphics/MatrixEngine/Camera3D.cpp
@@ -54,8 +54,8 @@ namespace MatrixEngine {
 			/*glm::vec3 translate(-_position.x + _screenWidth / 2, -_position.y + _screenHeight / 2, -_position.z);
 			_cameraMatrix = glm::translate(_orthoMatrix, translate);*/
 			//TODO: Check what is the proper value of Z for translation.
-			glm::vec3 translate(-_position.x + _screenWidth / 2, -_position.y + _screenHeight / 2, 0.0f);
-			_cameraMatrix = glm::translate(_cameraMatrix, translate);
+			//glm::vec3 translate(-_position.x + _screenWidth / 2, -_position.y + _screenHeight / 2, 0.0f);
+			//_cameraMatrix = glm::translate(_cameraMatrix, translate);
 			// Scale
 			glm::vec3 scale(_scale, _scale, _scale);
 			_cameraMatrix = glm::scale(glm::mat4(1.0f), scale) * _cameraMatrix;
