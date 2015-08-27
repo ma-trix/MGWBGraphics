0ccdd3e5479046b4b79bc9c1a475f80e387e68ff
Mateusz Łukomski <matrix.disc@gmail.com>
2015-08-27 09:15:05 +0200

    Draw voxel, added drawing axes.

--- MGWBGraphics/Rubik/RubikGame.cpp
+++ MGWBGraphics/Rubik/RubikGame.cpp
@@ -85,6 +85,76 @@ void setVertexData(MatrixEngine::Vertex3D* vertex, float x, float y, float z, fl
 	vertex->setUV(u, v);
 }
 
+void drawAxes()
+{
+	//std::string texturePath = "Textures/PNG/RedSquare80x80.png";
+	GLuint _vboID = 0;
+	GLuint _vao = 0;
+	float x = 0.0f;
+	float y = 0.0f;
+	float z = 0.0f;
+	float w = 3.0f;
+	float h = 3.0f;
+	float d = 3.0f;
+	//MatrixEngine::GLTexture _texture = MatrixEngine::ResourceManager::getTexture(texturePath);
+
+	if (_vboID == 0)
+	{
+		glGenBuffers(1, &_vboID);
+	}
+
+	MatrixEngine::Vertex3D vertexData[6];
+
+	//first 
+	vertexData[0].setPosition(x, y, z);
+	vertexData[1].setPosition(x + w, y, z);
+
+	vertexData[2].setPosition(x, y, z);
+	vertexData[3].setPosition(x, y + h, z);
+
+	vertexData[4].setPosition(x, y, z);
+	vertexData[5].setPosition(x, y, z + w);
+
+
+	vertexData[0].setColor(255, 0, 0, 255);
+	vertexData[1].setColor(255, 0, 0, 255);
+	vertexData[2].setColor(0, 255, 0, 255);
+	vertexData[3].setColor(0, 255, 0, 255);
+	vertexData[4].setColor(0, 0, 255, 255);
+	vertexData[5].setColor(0, 0, 255, 255);
+
+	glBindBuffer(GL_ARRAY_BUFFER, _vboID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	//unbinding buffer, 0 means no buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	if (_vao == 0)
+	{
+		glGenVertexArrays(1, &_vao);
+	}
+	glBindVertexArray(_vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, _vboID); //TODO: added via comparison to Sprite, not sure if it's correct
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(MatrixEngine::Vertex3D), (void*)offsetof(MatrixEngine::Vertex3D, position));
+	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(MatrixEngine::Vertex3D), (void*)offsetof(MatrixEngine::Vertex3D, color));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(MatrixEngine::Vertex3D), (void*)offsetof(MatrixEngine::Vertex3D, uv));
+
+	//glBindTexture(GL_TEXTURE_2D, _texture.id);
+
+	glDrawArrays(GL_LINES, 0, 6);
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+}
+
 void RubikGame::drawGame()
 {
 	glEnable(GL_DEPTH_TEST);
@@ -120,85 +190,12 @@ void RubikGame::drawGame()
 
 
 	MatrixEngine::Voxel voxel;
-	voxel.init(-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, "Textures/PNG/HeartAyse800x800.png");
+	voxel.init(0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f, "Textures/PNG/HeartAyse800x800.png");
 	voxel.draw();
 
 	///////////////////////
-	/*std::string texturePath = "Textures/PNG/RedSquare80x80.png";
-	GLuint _vboID = 0;
-	GLuint _vao = 0;
-	float x = -0.5f;
-	float y = -0.5f;
-	float z = -0.5f;
-	float width = 10.0f;
-	float height = 10.0f;
-	MatrixEngine::GLTexture _texture = MatrixEngine::ResourceManager::getTexture(texturePath);
-
-	if (_vboID == 0)
-	{
-		glGenBuffers(1, &_vboID);
-	}
-
-	MatrixEngine::Vertex3D vertexData[6];
-
-	//first triangle
-	vertexData[0].setPosition(-0.5f, -0.5f, -0.5f);
-	vertexData[0].setUV(0.0f, 0.0f);
-
-	vertexData[1].setPosition(0.5f, -0.5f, -0.5f);
-	vertexData[1].setUV(1.0f, 0.0f);
-
-	vertexData[2].setPosition(0.5f, 0.5f, -0.5f);
-	vertexData[2].setUV(1.0f, 1.0f);
-
-	//second triangle
-	vertexData[3].setPosition(0.5f, 0.5f, -0.5f);
-	vertexData[3].setUV(1.0f, 1.0f);
-
-	vertexData[4].setPosition(-0.5f, 0.5f, -0.5f);
-	vertexData[4].setUV(0.0f, 1.0f);
-
-	vertexData[5].setPosition(-0.5f, -0.5f, -0.5f);
-	vertexData[5].setUV(0.0f, 0.0f);
-
-	for (int i = 0; i < 6; i++)
-	{
-		vertexData[i].setColor(255, 255, 255, 255);
-	}
-
-	vertexData[1].setColor(0, 0, 255, 255);
-	vertexData[4].setColor(0, 255, 0, 255);
-
-	glBindBuffer(GL_ARRAY_BUFFER, _vboID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
-	//unbinding buffer, 0 means no buffer
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	if (_vao == 0)
-	{
-		glGenVertexArrays(1, &_vao);
-	}
-	glBindVertexArray(_vao);
-
-	glBindBuffer(GL_ARRAY_BUFFER, _vboID); //TODO: added via comparison to Sprite, not sure if it's correct
-
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(MatrixEngine::Vertex3D), (void*)offsetof(MatrixEngine::Vertex3D, position));
-	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(MatrixEngine::Vertex3D), (void*)offsetof(MatrixEngine::Vertex3D, color));
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(MatrixEngine::Vertex3D), (void*)offsetof(MatrixEngine::Vertex3D, uv));
-
-	glBindTexture(GL_TEXTURE_2D, _texture.id);
-
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDisableVertexAttribArray(0);
-	glDisableVertexAttribArray(1);
-	glDisableVertexAttribArray(2);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glBindVertexArray(0);*/
+	
+	drawAxes();
 
 	/////////////////////////////
 
